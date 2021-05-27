972bfc47b24593fa3a3ba457be1289a60389c12a
Sachit Lumba <sachitlumba@gmail.com>
2021-05-27 17:29:17 +0400

    add page not found for invalid routes

--- src/webpages/index.js
+++ src/webpages/index.js
@@ -3,6 +3,7 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect,
 } from "react-router-dom";
 
 import Home from './home/home';
@@ -11,10 +12,30 @@ function Webpages() {
     return(
       <Router>
         <Switch>
-          <Route path="/" component= {Home} />
+          <Route exact path="/" >
+              <Home />
+          </Route>
+          <Route path="/auth">
+              <Redirect to="/" />
+          </Route>
+          <Route path="*">
+              <NoMatch />
+          </Route>
         </Switch>
       </Router>
     );
 };
 
+function NoMatch() {
+    return (
+        <div>
+            <h1>
+                Error 404
+            </h1>
+            <p>
+                Page not found
+            </p>
+        </div>
+    )
+}
 export default Webpages;
