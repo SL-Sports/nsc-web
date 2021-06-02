b84f0d616be41dcde054ec3c1e2a63e2834fee4b
anudaweerasinghe <anuda@live.com>
2021-06-02 17:56:42 +0530

    basic get activities api call implemented

--- src/components/activities/index.js
+++ src/components/activities/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import activityService from '../../services/activityService';
 
-export default function Activites() {
+const profileId = '60ac7a65658e534fb80b9f52';
+
+export default function Activities() {
+
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        const getActivities = async () => {
+          const activitiesRes = await activityService.getActivities(profileId);
+          setActivities(activitiesRes.data);
+        }
+    
+        getActivities();
+      }, [])
+
+    // if(activitiesRes.status !== 200){
+    //     alert(activitiesRes.data);
+    // }else{
+    // }
+
     return (
         <>
             <h1>
-                Activities
+                Activities - Tiger Woods
             </h1>
+            <ul>
+                { activities.map(
+                    activity => 
+                        <li key={activity.activity._id}>{activity.activity.title}</li>
+                        )}
+            </ul>
         </>
     );
 }
