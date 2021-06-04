8e09dce8579e69d7552ffe3665d49180b5795141
anudaweerasinghe <anuda@live.com>
2021-06-04 20:02:37 +0530

    code style improved

--- src/components/activities/index.js
+++ src/components/activities/index.js
@@ -1,53 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import activityService from '../../services/activityService';
-import { AppBar, Typography, Toolbar, CssBaseline, Container, Grid} from '@material-ui/core';
-import Activity from './activityCard';
+import React, { useState, useEffect } from "react";
+import activityService from "../../services/activityService";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  CssBaseline,
+  Container,
+  Grid,
+} from "@material-ui/core";
+import Activity from "./activityCard";
 
-import {theme} from './activityTheme';
+import { theme } from "./activityTheme";
 
-const profileId = '60ac7a65658e534fb80b9f52';
-
-
+const profileId = "60ac7a65658e534fb80b9f52";
 
 export default function Activities() {
+  const [activities, setActivities] = useState([]);
 
+  useEffect(() => {
+    const getActivities = async () => {
+      const activitiesRes = await activityService.getActivities(profileId);
+      if (activitiesRes.status === 200) {
+        setActivities(activitiesRes.data);
+      }
+    };
 
-    const [activities, setActivities] = useState([]);
+    getActivities();
+  }, []);
 
-    
-
-    useEffect(() => {
-        const getActivities = async () => {
-          const activitiesRes = await activityService.getActivities(profileId);
-          if(activitiesRes.status === 200){
-            setActivities(activitiesRes.data);
-          }
-        }
-    
-        getActivities();
-      }, [])
-
-
-    return (
-        <>
-            <CssBaseline>
-                <AppBar style={{ background: theme.palette.primary.mainGradient }} position="relative" >
-                    <Toolbar>
-                    <Typography variant="h6" color="inherit" noWrap>
-                        Activities - Tiger Woods
-                    </Typography>
-                    </Toolbar>
-                </AppBar>
-                <main>
-                    <Container style={{paddingTop:30}} maxWidth="sm">
-                        <Grid container spacing={2}>
-                            {activities.map((activity) => (
-                            <Activity key={activity.activity.id} activity={activity}/>
-                            ))}
-                        </Grid>
-                    </Container>
-                </main>
-            </CssBaseline>
-        </>
-    );
+  return (
+    <>
+      <CssBaseline>
+        <AppBar
+          style={{ background: theme.palette.primary.mainGradient }}
+          position="relative"
+        >
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Activities - Tiger Woods
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Container style={{ paddingTop: 30 }} maxWidth="sm">
+            <Grid container spacing={2}>
+              {activities.map((activity) => (
+                <Activity key={activity.activity.id} activity={activity} />
+              ))}
+            </Grid>
+          </Container>
+        </main>
+      </CssBaseline>
+    </>
+  );
 }
--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import COLORS from '../../colors';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from "react-router-dom";
+import React from "react";
+import COLORS from "../../colors";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Activities from '../activities';
-import Profiles from '../profiles';
-import Payments from '../payments'
+import Activities from "../activities";
+import Profiles from "../profiles";
+import Payments from "../payments";
 
-function Home () {
-    return <h1  style={{color: COLORS.blueGradientStart}}>Home</h1>
+function Home() {
+  return <h1 style={{ color: COLORS.blueGradientStart }}>Home</h1>;
 }
 
-function NoMatch () {
-    return <h1>404 Page Not Found</h1>
+function NoMatch() {
+  return <h1>404 Page Not Found</h1>;
 }
 
-export default function Routes () {
-    return (
-        <Router>
-            <Switch>
-                {/* TODO: Auth */}
-                {/* Activities */}
-                <Route path='/activities' component={Activities} />
-                {/* Payments */}
-                <Route path='/payments' component={Payments} />
-                {/* Profiles */}
-                <Route path='/profiles' component={Profiles} />
-                {/* Home */}
-                <Route exact path='/' component={Home} />
-                {/* NoMatch */}
-                <Route path='*' component={NoMatch} />
-            </Switch>
-        </Router>
-    )
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        {/* TODO: Auth */}
+        {/* Activities */}
+        <Route path="/activities" component={Activities} />
+        {/* Payments */}
+        <Route path="/payments" component={Payments} />
+        {/* Profiles */}
+        <Route path="/profiles" component={Profiles} />
+        {/* Home */}
+        <Route exact path="/" component={Home} />
+        {/* NoMatch */}
+        <Route path="*" component={NoMatch} />
+      </Switch>
+    </Router>
+  );
 }
