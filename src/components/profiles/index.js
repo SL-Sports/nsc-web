e20ee15967d7b62bccb7ae1630c0e64843f3d591
Sachit Lumba <sachitlumba@gmail.com>
2021-06-04 14:54:22 +0400

    prettify

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -1,148 +1,150 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Paper, Grid, TextField } from '@material-ui/core';
-import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { Box, Paper, Grid, TextField } from "@material-ui/core";
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@material-ui/core";
 // eslint-disable-next-line
 import { spacing } from "@material-ui/system";
 
-import { getProfiles } from '../../services/profileService';
+import { getProfiles } from "../../services/profileService";
 
-function ProfileRow (props) {
-    const profile = props.profile;
+function ProfileRow(props) {
+  const profile = props.profile;
 
-    return (
-        <TableRow key={profile._id}>
-            <TableCell>{ profile.firstName }</TableCell>
-            <TableCell>{ profile.lastName }</TableCell>
-            <TableCell>{ profile.preferredName }</TableCell>
-            <TableCell>{ profile.profileType }</TableCell>
-        </TableRow>
-    )
+  return (
+    <TableRow key={profile._id}>
+      <TableCell>{profile.firstName}</TableCell>
+      <TableCell>{profile.lastName}</TableCell>
+      <TableCell>{profile.preferredName}</TableCell>
+      <TableCell>{profile.profileType}</TableCell>
+    </TableRow>
+  );
 }
 
-function ProfileFilterForm ({fields}) {
-    return (
-        <form>
-            <Grid container alignItems="center" justify="center">
-                <Grid item key="filter-label">
-                    <Box mx={1}>
-                        <h3>Filter:</h3>
-                    </Box>
-                </Grid>
-                { fields.map( ({ field, value, setter }) => 
-                    <Grid item key={field}>
-                        <Box mx={1}>
-                            <TextField 
-                            name={field} 
-                            label={field} 
-                            value={value}
-                            variant="outlined"
-                            onChange = { (event) => setter (event.target.value) } />
-                        </Box>
-                    </Grid>
-                )}
-            </Grid>
-        </form>
-
-    )
+function ProfileFilterForm({ fields }) {
+  return (
+    <form>
+      <Grid container alignItems="center" justify="center">
+        <Grid item key="filter-label">
+          <Box mx={1}>
+            <h3>Filter:</h3>
+          </Box>
+        </Grid>
+        {fields.map(({ field, value, setter }) => (
+          <Grid item key={field}>
+            <Box mx={1}>
+              <TextField
+                name={field}
+                label={field}
+                value={value}
+                variant="outlined"
+                onChange={(event) => setter(event.target.value)}
+              />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </form>
+  );
 }
 
-export default function Profiles () {
-    // Profile lists
-    let [profiles, setProfiles] = useState([]);
-    // eslint-disable-next-line
-    let [admins, setAdmins] = useState([]);
-    // eslint-disable-next-line
-    let [athletes, setAthletes] = useState([]);
-    // eslint-disable-next-line
-    let [coaches, setCoaches] = useState([]);
+export default function Profiles() {
+  // Profile lists
+  let [profiles, setProfiles] = useState([]);
+  // eslint-disable-next-line
+  let [admins, setAdmins] = useState([]);
+  // eslint-disable-next-line
+  let [athletes, setAthletes] = useState([]);
+  // eslint-disable-next-line
+  let [coaches, setCoaches] = useState([]);
 
-    // Form filter fields
-    let [firstName, setFirstName] = useState("");
-    let [lastName, setLastName] = useState("");
-    let fields = [
-        { field: "First Name", value: firstName, setter: setFirstName },
-        { field: "Last Name", value: lastName, setter: setLastName },
-    ];
+  // Form filter fields
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let fields = [
+    { field: "First Name", value: firstName, setter: setFirstName },
+    { field: "Last Name", value: lastName, setter: setLastName },
+  ];
 
-    useEffect(() => {
-        async function updateProfiles() {
-            const profilesResponse = await getProfiles(firstName, lastName);
-            console.log(profilesResponse);
-            if (profilesResponse.status === 200) {
-                // If request is good get profiles
-                const profiles = profilesResponse.data;
+  useEffect(() => {
+    async function updateProfiles() {
+      const profilesResponse = await getProfiles(firstName, lastName);
+      console.log(profilesResponse);
+      if (profilesResponse.status === 200) {
+        // If request is good get profiles
+        const profiles = profilesResponse.data;
 
-                // Set whole profiles list
-                setProfiles(profiles
-                    .sort(
-                    (p1, p2) => {
-                        return p1.profile.profileType.localeCompare(p2.profile.profileType);
-                    }
-                    )
-                );
+        // Set whole profiles list
+        setProfiles(
+          profiles.sort((p1, p2) => {
+            return p1.profile.profileType.localeCompare(p2.profile.profileType);
+          })
+        );
 
-                // Get admins
-                setAdmins(
-                    profiles.filter(
-                        (p) => {
-                            return p.profile.profileType === "ADMIN";
-                        }
-                    )
-                );
+        // Get admins
+        setAdmins(
+          profiles.filter((p) => {
+            return p.profile.profileType === "ADMIN";
+          })
+        );
 
-                // Get athletes
-                setAthletes(
-                    profiles.filter(
-                        (p) => {
-                            return p.profile.profileType === "ATHLETE";
-                        }
-                    )
-                );
+        // Get athletes
+        setAthletes(
+          profiles.filter((p) => {
+            return p.profile.profileType === "ATHLETE";
+          })
+        );
 
-                // Get coaches
-                setCoaches(
-                    profiles.filter(
-                        (p) => {
-                            return p.profile.profileType === "COACH";
-                        }
-                    )
-                );
-            }
-        }
+        // Get coaches
+        setCoaches(
+          profiles.filter((p) => {
+            return p.profile.profileType === "COACH";
+          })
+        );
+      }
+    }
 
-        updateProfiles();
+    updateProfiles();
 
-        // Why does just this not work?
-        // getProfiles().then(resp => {
-        //     console.log(resp);
-        // })
-    }, [firstName, lastName]);
+    // Why does just this not work?
+    // getProfiles().then(resp => {
+    //     console.log(resp);
+    // })
+  }, [firstName, lastName]);
 
-    return (
-        <>
-            <h1>
-                Profiles
-            </h1>
-            <Box name="profile-filter-form" my={2}>
-                <ProfileFilterForm fields = {fields} />
-            </Box>
-            <Box name="profile-list">
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>First Name</TableCell>
-                                <TableCell>Last Name</TableCell>
-                                <TableCell>Preferred Name</TableCell>
-                                <TableCell>Profile Type</TableCell>
-                            </TableRow>    
-                        </TableHead>
-                        <TableBody>
-                            { profiles.map(profile => <ProfileRow key={profile.profile._id} profile={profile.profile} />) }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <h1>Profiles</h1>
+      <Box name="profile-filter-form" my={2}>
+        <ProfileFilterForm fields={fields} />
+      </Box>
+      <Box name="profile-list">
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell>Last Name</TableCell>
+                <TableCell>Preferred Name</TableCell>
+                <TableCell>Profile Type</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {profiles.map((profile) => (
+                <ProfileRow
+                  key={profile.profile._id}
+                  profile={profile.profile}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
 }
--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,59 +1,65 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = 'https://slsports.anuda.me/profile';
-const token = 'omHdMoX8HVao6Ui6aTfLhg8m6CNY6eNqazJFcWOl63fiJwbPg8PFYVa836xuL9uzgjhpmEuQjSDYUZFIsKg9ZN6oARoFJu5Vxl59rfOVmS19jBUqYvWVnmv4NwGaf2GX';
+const baseUrl = "https://slsports.anuda.me/profile";
+const token =
+  "omHdMoX8HVao6Ui6aTfLhg8m6CNY6eNqazJFcWOl63fiJwbPg8PFYVa836xuL9uzgjhpmEuQjSDYUZFIsKg9ZN6oARoFJu5Vxl59rfOVmS19jBUqYvWVnmv4NwGaf2GX";
 
 /**
  * getProfiles:
  *  fetches profiles filtered by the input params
  *  if they are supplied
- * 
+ *
  * @param {string} firstName first name of a profile
  * @param {string} lastName  last name of a profile
  * @return {Object}          on success, result has the same status
  *                            and data as the API response
  *                           on failure, result has the same status
- *                            as the error, with data containing 
+ *                            as the error, with data containing
  *                            the error message
- * 
+ *
  */
-export async function getProfiles (firstName = "", lastName = "", preferredName = "") {
-    const url = baseUrl + '/get';
+export async function getProfiles(
+  firstName = "",
+  lastName = "",
+  preferredName = ""
+) {
+  const url = baseUrl + "/get";
 
-    let body = {};
+  let body = {};
 
-    // add keys to body if they have been specified
-    if (firstName !== "") body.firstName = firstName;
-    if (lastName !== "") body.lastName = lastName;
-    if (preferredName !== "") body.preferredName = preferredName;
+  // add keys to body if they have been specified
+  if (firstName !== "") body.firstName = firstName;
+  if (lastName !== "") body.lastName = lastName;
+  if (preferredName !== "") body.preferredName = preferredName;
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Token': token
-        }
-    };
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Token: token,
+    },
+  };
 
-    let result = {};
+  let result = {};
 
-    await axios.post(url, body, config)
-        .then(response => {
-            result.status = response.status;
+  await axios
+    .post(url, body, config)
+    .then((response) => {
+      result.status = response.status;
 
-            if (response.status === 200) {
-                result.data = response.data;
-            } else {
-                result.data = response.data.message;
-            }
+      if (response.status === 200) {
+        result.data = response.data;
+      } else {
+        result.data = response.data.message;
+      }
 
-            // return result;
-        })
-        .catch(err => {
-            result.status = err.response.status;
-            result.data = err.response.data.message;
+      // return result;
+    })
+    .catch((err) => {
+      result.status = err.response.status;
+      result.data = err.response.data.message;
 
-            // return result;
-        });
+      // return result;
+    });
 
-    return result;
-};
+  return result;
+}
