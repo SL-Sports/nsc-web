9b138bbdec5ff3ed69aa4873d870775d81aa53f8
Sachit Lumba <sachitlumba@gmail.com>
2021-06-04 12:56:53 +0400

    add server-side filtering on field change

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Paper } from '@material-ui/core';
+import { Box, Paper, Grid, TextField } from '@material-ui/core';
 import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+// eslint-disable-next-line
+import { spacing } from "@material-ui/system";
 
 import { getProfiles } from '../../services/profileService';
 
@@ -17,22 +19,95 @@ function ProfileRow (props) {
     )
 }
 
-export default function Profiles () {
-    // eslint-disable-next-line
-    let [firstName, setFirstName] = useState("");
-    // eslint-disable-next-line
-    let [lastName, setLastName] = useState("");
+function ProfileFilterForm ({fields}) {
+    return (
+        <form>
+            <Grid container alignItems="center" justify="center">
+                <Grid item key="filter-label">
+                    <Box mx={1}>
+                        <h3>Filter:</h3>
+                    </Box>
+                </Grid>
+                { fields.map( ({ field, value, setter }) => 
+                    <Grid item key={field}>
+                        <Box mx={1}>
+                            <TextField 
+                            name={field} 
+                            label={field} 
+                            value={value}
+                            variant="outlined"
+                            onChange = { (event) => setter (event.target.value) } />
+                        </Box>
+                    </Grid>
+                )}
+            </Grid>
+        </form>
 
+    )
+}
+
+export default function Profiles () {
+    // Profile lists
     let [profiles, setProfiles] = useState([]);
+    // eslint-disable-next-line
+    let [admins, setAdmins] = useState([]);
+    // eslint-disable-next-line
+    let [athletes, setAthletes] = useState([]);
+    // eslint-disable-next-line
+    let [coaches, setCoaches] = useState([]);
+
+    // Form filter fields
+    let [firstName, setFirstName] = useState("");
+    let [lastName, setLastName] = useState("");
+    let fields = [
+        { field: "First Name", value: firstName, setter: setFirstName },
+        { field: "Last Name", value: lastName, setter: setLastName },
+    ];
 
     useEffect(() => {
         async function updateProfiles() {
-            const profilesResponse = await getProfiles();
-            setProfiles(profilesResponse.data.sort(
-                (p1, p2) => {
-                    return p1.profile.profileType.localeCompare(p2.profile.profileType);
-                }
-            ));
+            const profilesResponse = await getProfiles(firstName, lastName);
+            console.log(profilesResponse);
+            if (profilesResponse.status === 200) {
+                // If request is good get profiles
+                const profiles = profilesResponse.data;
+
+                // Set whole profiles list
+                setProfiles(profiles
+                    .sort(
+                    (p1, p2) => {
+                        return p1.profile.profileType.localeCompare(p2.profile.profileType);
+                    }
+                    )
+                );
+
+                // Get admins
+                setAdmins(
+                    profiles.filter(
+                        (p) => {
+                            return p.profile.profileType === "ADMIN";
+                        }
+                    )
+                );
+
+                // Get athletes
+                setAthletes(
+                    profiles.filter(
+                        (p) => {
+                            return p.profile.profileType === "ATHLETE";
+                        }
+                    )
+                );
+
+                // Get coaches
+                setCoaches(
+                    profiles.filter(
+                        (p) => {
+                            return p.profile.profileType === "COACH";
+                        }
+                    )
+                );
+            }
         }
 
         updateProfiles();
@@ -41,14 +116,17 @@ export default function Profiles () {
         // getProfiles().then(resp => {
         //     console.log(resp);
         // })
-    }, []);
+    }, [firstName, lastName]);
 
     return (
         <>
             <h1>
                 Profiles
             </h1>
-            <Box className="profile-list">
+            <Box name="profile-filter-form" my={2}>
+                <ProfileFilterForm fields = {fields} />
+            </Box>
+            <Box name="profile-list">
                 <TableContainer component={Paper}>
                     <Table>
                         <TableHead>
