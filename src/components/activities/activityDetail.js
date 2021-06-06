f7b39a0999adb92a605969620d868e76ec96d56f
anudaweerasinghe <anuda@live.com>
2021-06-06 12:16:31 +0530

    added circular progress loader to activity detail screen

--- src/components/activities/activityDetail.js
+++ src/components/activities/activityDetail.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import activityService from "../../services/activityService";
 import { useParams } from "react-router-dom";
+import { CircularProgress, Grid } from "@material-ui/core";
+import COLORS from "../../colors";
+import { theme, useStyles } from "./activityTheme";
 
 export default function ActivityDetail() {
   const { activityId } = useParams();
@@ -20,13 +23,31 @@ export default function ActivityDetail() {
     };
 
     getActivity();
-  }, []);
-  return (
-    <>
-      <h1>
-        Activity Detail -{" "}
-        {activity !== undefined ? activity.activity.title : activityId}
-      </h1>
-    </>
-  );
+  });
+  if (activity === undefined) {
+    return (
+      <>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item xs={3}>
+            <CircularProgress
+              style={{ color: theme.palette.primary.main, margin: "auto" }}
+            ></CircularProgress>{" "}
+          </Grid>
+        </Grid>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Activity Detail - {activity.activity.title}</h1>
+      </>
+    );
+  }
 }
