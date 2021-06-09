4cb43dc833818288b06719a561d43558dfcb609c
Sachit Lumba <sachitlumba@gmail.com>
2021-06-09 12:06:45 +0400

    more paper removal

--- src/components/profiles/components/profileDisplay.js
+++ src/components/profiles/components/profileDisplay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Paper, Grid, Box, Card, Avatar, Typography } from "@material-ui/core";
+import { Grid, Box, Card, Avatar, Typography } from "@material-ui/core";
 // eslint-disable-next-line
 import { spacing } from "@material-ui/system";
 
@@ -32,17 +32,17 @@ export function ProfileDisplay({ profileHeader }) {
         </Box>
         <CoachesStudentsList profileHeader={profileHeader} />
       </Grid>
-      <Grid item name="rankings" xs={12} md={6}>
+      <Grid item name="rankings" xs={12} md={5}>
         <Box m={3} p={3}>
           <Typography variant="h4">Rankings</Typography>
         </Box>
       </Grid>
-      <Grid item name="activities" xs={12} md={6}>
+      <Grid item name="activities" xs={12} md={5}>
         <Box m={3} p={3}>
           <Typography variant="h4">Activities</Typography>
         </Box>
       </Grid>
-      <Grid item name="payments" xs={12} md={6}>
+      <Grid item name="payments" xs={12} md={5}>
         <Box m={3} p={3}>
           <Typography variant="h4">Payments</Typography>
         </Box>
@@ -53,62 +53,58 @@ export function ProfileDisplay({ profileHeader }) {
 
 function ProfileDetails({ profile }) {
   return (
-    <Paper>
-      <Box m={3} p={3}>
-        <Grid container direction="column" spacing={2}>
-          <Grid item>
-            <Typography variant="h4">Profile Information</Typography>
-          </Grid>
-          <Grid item>
-            <Card raised style={{ borderRadius: 20 }}>
-              <Box m={3}>
-                <Grid container alignItems="center" spacing={2}>
-                  <Grid item xs={12} md={3} align="center">
-                    <Avatar
-                      src={profile.profilePicUrl}
-                      style={{ width: 80, height: 80 }}
-                    />
-                  </Grid>
-                  <Grid item xs={8} md={6}>
-                    <Typography variant="h4">
-                      {profile.preferredName} {profile.lastName}
-                    </Typography>
-                    <Typography variant="subtitle1">
-                      {title(profile.profileType)}
-                    </Typography>
-                    <Typography variant="subtitle2">
-                      {profile.country} {profile.sport.name}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={4} md={3} align="center">
-                    <Typography>Age: {profile.age}</Typography>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card style={{ borderRadius: 20 }}>
-              <Box p={2} align="left">
-                <Typography variant="h6">Secondary Information</Typography>
-                <Typography>
-                  Full Name: {profile.firstName} {profile.lastName}
-                </Typography>
-                <Typography>
-                  Date of Birth: {getDOB(profile.dateOfBirth)}
-                </Typography>
-                <Typography variant="body1">
-                  School: {profile.school}
-                </Typography>
-                <Typography>
-                  City, Country: {profile.city}, {profile.country}
-                </Typography>
-              </Box>
-            </Card>
-          </Grid>
+    <Box m={3} p={3}>
+      <Grid container direction="column" spacing={2}>
+        <Grid item>
+          <Typography variant="h4">Profile Information</Typography>
         </Grid>
-      </Box>
-    </Paper>
+        <Grid item>
+          <Card raised style={{ borderRadius: 20 }}>
+            <Box m={3}>
+              <Grid container alignItems="center" spacing={2}>
+                <Grid item xs={12} md={3} align="center">
+                  <Avatar
+                    src={profile.profilePicUrl}
+                    style={{ width: 80, height: 80 }}
+                  />
+                </Grid>
+                <Grid item xs={8} md={6}>
+                  <Typography variant="h4">
+                    {profile.preferredName} {profile.lastName}
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    {title(profile.profileType)}
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    {profile.country} {profile.sport.name}
+                  </Typography>
+                </Grid>
+                <Grid item xs={4} md={3} align="center">
+                  <Typography>Age: {profile.age}</Typography>
+                </Grid>
+              </Grid>
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card style={{ borderRadius: 20 }}>
+            <Box p={2} align="left">
+              <Typography variant="h6">Secondary Information</Typography>
+              <Typography>
+                Full Name: {profile.firstName} {profile.lastName}
+              </Typography>
+              <Typography>
+                Date of Birth: {getDOB(profile.dateOfBirth)}
+              </Typography>
+              <Typography variant="body1">School: {profile.school}</Typography>
+              <Typography>
+                City, Country: {profile.city}, {profile.country}
+              </Typography>
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
