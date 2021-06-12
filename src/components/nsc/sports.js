9e877f0429dc677b5411e3fa50a69a6198293549
anudaweerasinghe <anuda@live.com>
2021-06-12 14:47:17 +0530

    added circular progress to sports view

--- src/components/nsc/sports.js
+++ src/components/nsc/sports.js
@@ -21,7 +21,7 @@ import Sport from "./sportCard";
 
 export default function Sports() {
   const classes = useStyles();
-  const [sports, setSports] = useState([]);
+  const [sports, setSports] = useState(undefined);
   const [saving, setSaving] = useState(false);
   const [editing, setEditing] = useState(false);
   const [name, setName] = useState("");
@@ -37,89 +37,128 @@ export default function Sports() {
     loadSports();
   }, []);
 
-  return (
-    <Grid
-      container
-      spacing={2}
-      alignContent="center"
-      alignItems="center"
-      justify="center"
-    >
-      <Grid item xs={10}>
-        <Typography variant="h4" align="left" style={{ fontWeight: "bolder" }}>
-          Sports
-        </Typography>
-      </Grid>
-      <Grid item xs={2}>
-        <IconButton
-          color={editing ? "secondary" : "primary"}
-          aria-label="new-comment"
-          size="medium"
-          onClick={() => setEditing(!editing)}
-        >
-          {editing ? (
-            <Close fontSize="large"></Close>
-          ) : (
-            <Add fontSize="large"></Add>
-          )}
-        </IconButton>
-      </Grid>
-      {editing && (
+  if (sports === undefined) {
+    return (
+      <Grid
+        container
+        spacing={2}
+        alignContent="center"
+        alignItems="center"
+        justify="center"
+      >
         <Grid item xs={12}>
-          <TextField
-            color="primary"
-            required
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            autoComplete="new sport name"
-            autoFocus
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Grid>
-      )}
-      {editing && (
-        <Grid item xs={12}>
-          <TextField
-            color="primary"
-            required
-            fullWidth
-            id="description"
-            label="Description"
-            name="description"
-            autoComplete="new sport description"
-            multiline
-            autoFocus
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </Grid>
-      )}
-      {editing && (
-        <Grid item xs={12}>
-          <Button
-            style={{
-              background: theme.palette.primary.mainGradient,
-              color: "white",
-              borderRadius: 20,
-              fontWeight: "bolder",
-            }}
-            fullWidth
-            // onClick={saveComment}
+          <Typography
+            variant="h4"
+            align="left"
+            style={{ fontWeight: "bolder" }}
           >
-            {saving ? (
-              <CircularProgress style={{ color: "white" }}></CircularProgress>
-            ) : (
-              "SAVE"
-            )}
-          </Button>
+            Sports
+          </Typography>
         </Grid>
-      )}
-      {sports.map((sport) => (
-        <Sport key={sport._id} sport={sport} />
-      ))}
-    </Grid>
-  );
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "20vh" }}
+        >
+          <Grid item xs={3}>
+            <CircularProgress
+              style={{ color: theme.palette.primary.main, margin: "auto" }}
+            ></CircularProgress>{" "}
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  } else {
+    return (
+      <Grid
+        container
+        spacing={2}
+        alignContent="center"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item xs={10}>
+          <Typography
+            variant="h4"
+            align="left"
+            style={{ fontWeight: "bolder" }}
+          >
+            Sports
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <IconButton
+            color={editing ? "secondary" : "primary"}
+            aria-label="new-comment"
+            size="medium"
+            onClick={() => setEditing(!editing)}
+          >
+            {editing ? (
+              <Close fontSize="large"></Close>
+            ) : (
+              <Add fontSize="large"></Add>
+            )}
+          </IconButton>
+        </Grid>
+        {editing && (
+          <Grid item xs={12}>
+            <TextField
+              color="primary"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="new sport name"
+              autoFocus
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Grid>
+        )}
+        {editing && (
+          <Grid item xs={12}>
+            <TextField
+              color="primary"
+              fullWidth
+              id="description"
+              label="Description"
+              name="description"
+              autoComplete="new sport description"
+              multiline
+              autoFocus
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Grid>
+        )}
+        {editing && (
+          <Grid item xs={12}>
+            <Button
+              style={{
+                background: theme.palette.primary.mainGradient,
+                color: "white",
+                borderRadius: 20,
+                fontWeight: "bolder",
+              }}
+              fullWidth
+              // onClick={saveComment}
+            >
+              {saving ? (
+                <CircularProgress style={{ color: "white" }}></CircularProgress>
+              ) : (
+                "SAVE"
+              )}
+            </Button>
+          </Grid>
+        )}
+        {sports.map((sport) => (
+          <Sport key={sport._id} sport={sport} />
+        ))}
+      </Grid>
+    );
+  }
 }
