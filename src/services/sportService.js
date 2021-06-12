3b9f7c4368299f208a86a8b0394bc5bdac0f939e
anudaweerasinghe <anuda@live.com>
2021-06-12 13:00:07 +0530

    implemented sports api calls in sportService

---
+++ src/services/sportService.js
@@ -0,0 +1,182 @@
+import axios from "axios";
+
+const baseUrl = "https://slsports.anuda.me/sport";
+const token =
+  "xIPkeAkS8PDP2iNSUcFpDFcTYaO5dyJUYxEySAAIO0IAxL9jYfxcmwYocXuVN5fdEpyS6er6Lbp9dtqlYVOJOFs39JXDJwnOyygpUQ74bDVhZCf8cbQu29KbTVMDsPTJ";
+
+export async function getSports() {
+  const url = baseUrl + "/get";
+  const body = {};
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  let result = {};
+
+  await axios
+    .post(url, body, config)
+    .then((response) => {
+      result.status = response.status;
+      if (response.status === 200) {
+        result.data = response.data;
+      } else {
+        if (response.data.message !== undefined) {
+          result.data = response.data.message;
+        } else {
+          result.data =
+            "We encountered an error while retrieving sports data. Please try again.";
+        }
+        alert(result.data);
+      }
+    })
+    .catch((err) => {
+      result.status = err.response.status;
+      if (err.response.data.message !== undefined) {
+        result.data = err.response.data.message;
+      } else {
+        result.data =
+          "We encountered an error while retrieving sports data. Please try again.";
+      }
+      alert(result.data);
+    });
+
+  return result;
+}
+
+export async function getSportById(id) {
+  const url = baseUrl + "/get";
+  const body = {
+    _id: id,
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  let result = {};
+
+  await axios
+    .post(url, body, config)
+    .then((response) => {
+      result.status = response.status;
+      if (response.status === 200) {
+        result.data = response.data;
+      } else {
+        if (response.data.message !== undefined) {
+          result.data = response.data.message;
+        } else {
+          result.data =
+            "We encountered an error while retrieving sports data. Please try again.";
+        }
+        alert(result.data);
+      }
+    })
+    .catch((err) => {
+      result.status = err.response.status;
+      if (err.response.data.message !== undefined) {
+        result.data = err.response.data.message;
+      } else {
+        result.data =
+          "We encountered an error while retrieving sports data. Please try again.";
+      }
+      alert(result.data);
+    });
+
+  return result;
+}
+
+export async function addSport(name, description) {
+  const url = baseUrl + "/new";
+  const body = {
+    name: name,
+    description: description,
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Token: token,
+    },
+  };
+
+  let result = {};
+
+  await axios
+    .post(url, body, config)
+    .then((response) => {
+      result.status = response.status;
+      if (response.status === 200) {
+        result.data = response.data.message;
+      } else {
+        if (response.data.message !== undefined) {
+          result.data = response.data.message;
+        } else {
+          result.data =
+            "We encountered an error while adding this sport. Please try again.";
+        }
+      }
+    })
+    .catch((err) => {
+      result.status = err.response.status;
+      if (err.response.data.message !== undefined) {
+        result.data = err.response.data.message;
+      } else {
+        result.data =
+          "We encountered an error while adding this sport. Please try again.";
+      }
+    });
+
+  alert(result.data);
+  return result;
+}
+
+export async function editSport(id, name, description) {
+  const url = baseUrl + "/edit";
+  const body = {
+    name: name,
+    description: description,
+    _id: id,
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Token: token,
+    },
+  };
+
+  let result = {};
+
+  await axios
+    .post(url, body, config)
+    .then((response) => {
+      result.status = response.status;
+      if (response.status === 200) {
+        result.data = response.data.message;
+      } else {
+        if (response.data.message !== undefined) {
+          result.data = response.data.message;
+        } else {
+          result.data =
+            "We encountered an error while editing this sport. Please try again.";
+        }
+      }
+    })
+    .catch((err) => {
+      result.status = err.response.status;
+      if (err.response.data.message !== undefined) {
+        result.data = err.response.data.message;
+      } else {
+        result.data =
+          "We encountered an error while editing this sport. Please try again.";
+      }
+    });
+
+  alert(result.data);
+  return result;
+}
