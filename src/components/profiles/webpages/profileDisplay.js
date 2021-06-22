e1dd971d301abcfb40a6407b9441cc5e4a60835d
anudaweerasinghe <anuda@live.com>
2021-06-22 16:35:32 +0530

    Removed profile info title in profile detail page

--- src/components/profiles/webpages/profileDisplay.js
+++ src/components/profiles/webpages/profileDisplay.js
@@ -11,8 +11,6 @@ import {
 } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 import { Link } from "react-router-dom";
-// eslint-disable-next-line
-
 import { getDOB } from "../../../services/dateService";
 import { ProfileList } from "../components/profileList";
 import NavBar from "../../navbar";
@@ -143,77 +141,72 @@ export function ProfileDisplay() {
 
 function ProfileDetails({ profile }) {
   return (
-    <Box m={3} p={3}>
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <Typography variant="h4">Profile Information</Typography>
-        </Grid>
-        <Grid item>
-          <Card raised style={{ borderRadius: 20 }}>
-            <Box m={3}>
-              <Grid container alignItems="center" spacing={2}>
-                <Grid item xs={12} md={3} align="center">
-                  <Avatar
-                    src={profile.profilePicUrl}
-                    style={{ width: 80, height: 80 }}
-                  />
-                </Grid>
-                <Grid item xs={8} md={6}>
-                  <Typography variant="h4">
-                    {profile.preferredName} {profile.lastName}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    {title(profile.profileType)}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                    {profile.country} {profile.sport.name}
-                  </Typography>
-                </Grid>
-                <Grid item xs={4} md={3} align="center">
-                  <Typography>Age: {profile.age}</Typography>
-                </Grid>
+    <Grid container direction="column" spacing={2}>
+      <Grid item>
+        <Card raised style={{ borderRadius: 20 }}>
+          <Box m={3}>
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item xs={12} md={3} align="center">
+                <Avatar
+                  src={profile.profilePicUrl}
+                  style={{ width: 80, height: 80 }}
+                />
               </Grid>
-            </Box>
-          </Card>
-        </Grid>
-        <Grid item>
-          <Grid container spacing={2} justify="center">
-            <Grid item>
-              <Link
-                to={`/profiles/edit/${profile._id}`}
-                style={{ textDecoration: "none" }}
-              >
-                <Button variant="contained" color="secondary">
-                  Edit Profile
-                </Button>
-              </Link>
+              <Grid item xs={8} md={6}>
+                <Typography variant="h4">
+                  {profile.preferredName} {profile.lastName}
+                </Typography>
+                <Typography variant="subtitle1">
+                  {title(profile.profileType)}
+                </Typography>
+                <Typography variant="subtitle2">
+                  {profile.country} {profile.sport.name}
+                </Typography>
+              </Grid>
+              <Grid item xs={4} md={3} align="center">
+                <Typography>Age: {profile.age}</Typography>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Button variant="contained" color="primary">
-                Invite
+          </Box>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Grid container spacing={2} justify="center">
+          <Grid item>
+            <Link
+              to={`/profiles/edit/${profile._id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <Button variant="contained" color="secondary">
+                Edit Profile
               </Button>
-            </Grid>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary">
+              Invite
+            </Button>
           </Grid>
         </Grid>
-        <Grid item>
-          <Card style={{ borderRadius: 20 }}>
-            <Box p={2} align="left">
-              <Typography variant="h6">Secondary Information</Typography>
-              <Typography>
-                Full Name: {profile.firstName} {profile.lastName}
-              </Typography>
-              <Typography>
-                Date of Birth: {getDOB(profile.dateOfBirth)}
-              </Typography>
-              <Typography variant="body1">School: {profile.school}</Typography>
-              <Typography>
-                City, Country: {profile.city}, {profile.country}
-              </Typography>
-            </Box>
-          </Card>
-        </Grid>
       </Grid>
-    </Box>
+      <Grid item>
+        <Card style={{ borderRadius: 20 }}>
+          <Box p={2} align="left">
+            <Typography variant="h6">Secondary Information</Typography>
+            <Typography>
+              Full Name: {profile.firstName} {profile.lastName}
+            </Typography>
+            <Typography>
+              Date of Birth: {getDOB(profile.dateOfBirth)}
+            </Typography>
+            <Typography variant="body1">School: {profile.school}</Typography>
+            <Typography>
+              City, Country: {profile.city}, {profile.country}
+            </Typography>
+          </Box>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
