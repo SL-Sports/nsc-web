b36286737f0bafa11093762d269c37d274ead5d3
anudaweerasinghe <anuda@live.com>
2021-06-24 17:20:24 +0530

    added themeprovider to profiles module

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -1,4 +1,4 @@
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -11,29 +11,35 @@ import EditProfile from "./webpages/editProfile";
 import NewCoach from "./webpages/newCoach";
 import SendInvite from "./webpages/sendInvite";
 import EditCoach from "./webpages/editCoach";
-
+import { theme } from "./profilesTheme";
 export default function Profiles() {
   return (
     <Router>
       <CssBaseline />
-      <Switch>
-        <Route exact path="/profiles" component={ProfilesHome} />
-        <Route exact path="/profiles/new" component={NewProfile} />
-        <Route exact path="/profiles/edit/:profileID" component={EditProfile} />
-        <Route
-          exact
-          path="/profiles/invite/:profileID"
-          component={SendInvite}
-        />
-        <Route exact path="/profiles/coaches/new" component={NewCoach} />
-        <Route
-          exact
-          path="/profiles/coaches/edit/:coachID"
-          component={EditCoach}
-        />
-        <Route exact path="/profiles/:profileID" component={ProfileDisplay} />
-        <Route component={Routes} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/profiles" component={ProfilesHome} />
+          <Route exact path="/profiles/new" component={NewProfile} />
+          <Route
+            exact
+            path="/profiles/edit/:profileID"
+            component={EditProfile}
+          />
+          <Route
+            exact
+            path="/profiles/invite/:profileID"
+            component={SendInvite}
+          />
+          <Route exact path="/profiles/coaches/new" component={NewCoach} />
+          <Route
+            exact
+            path="/profiles/coaches/edit/:coachID"
+            component={EditCoach}
+          />
+          <Route exact path="/profiles/:profileID" component={ProfileDisplay} />
+          <Route component={Routes} />
+        </Switch>
+      </ThemeProvider>
     </Router>
   );
 }
