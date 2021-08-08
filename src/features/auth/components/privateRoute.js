5b4098b61fa4b715fb4fd1900613c8c9f56c1947
Sachit Lumba <44508004+Sparrownor1@users.noreply.github.com>
2021-08-08 18:52:25 +0400

    update privateRoute

--- src/features/auth/components/privateRoute.js
+++ src/features/auth/components/privateRoute.js
@@ -3,27 +3,15 @@ import { Route, Redirect } from "react-router-dom";
 
 import { useAuth } from "../hooks/useAuth";
 
-export function PrivateRoute({ agent, client, children, ...rest }) {
+export function PrivateRoute({ children, ...rest }) {
   let auth = useAuth();
-
-  const pagePermissions =
-    auth.user &&
-    ((auth.user.isAgent && agent) || (auth.user.isClient && client));
 
   return (
     <Route
       {...rest}
       render={({ location }) => {
         return auth.user ? ( // Check if logged in
-          pagePermissions ? ( // Check permissions
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/",
-              }}
-            />
-          )
+          children
         ) : (
           <Redirect
             to={{
