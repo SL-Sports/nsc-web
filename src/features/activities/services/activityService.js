574c8fddae3d0e1a27e3d952f230d57bcfcdc8ba
Sachit Lumba <sachitlumba@gmail.com>
2021-08-18 21:02:47 +0400

    activityService done

--- src/features/activities/services/activityService.js
+++ src/features/activities/services/activityService.js
@@ -1,281 +1,71 @@
-import axios from "axios";
-import authService from "../../../services/authService";
+import { postHTTP } from "../../../helpers/http";
 
-const baseUrl = "https://slsports.anuda.me/activity";
+class ActivityService {
+  static getActivities = (profileId) => {
+    const data = {
+      profile: profileId,
+    };
 
-const getActivities = async (profileId) => {
-  let token = await authService.getToken();
-  let accountType = await authService.getAccountType();
-  const url = baseUrl + "/get";
-
-  const body = {
-    profile: profileId,
+    return postHTTP("/activity/get", data, { withCredentials: true });
   };
 
-  if (accountType === "ASSOCIATION_ADMIN") {
-    body.association = await authService.getActiveAssociationID();
-  }
+  static getActivityDetail = (activityId) => {
+    const data = {
+      _id: activityId,
+    };
 
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Token: token,
-    },
+    return postHTTP("/activity/get", data, { withCredentials: true });
   };
 
-  let result = {};
+  static approveActivity = (activityId, approvedBy) => {
+    const data = {
+      activityId: activityId,
+      approvedBy: approvedBy,
+    };
 
-  let response = await axios.post(url, body, config).catch((err) => {
-    result.status = err.response.status;
-    result.data = err.response.data.message;
-    alert(result.data);
-    return result;
-  });
-  result.status = response.status;
-
-  if (response.status === 200) {
-    result.data = response.data;
-  } else {
-    result.data = response.data.message;
-  }
-
-  return result;
-};
-
-const getActivityDetail = async (activityId) => {
-  let token = await authService.getToken();
-  let accountType = await authService.getAccountType();
-  const url = baseUrl + "/get";
-  const body = {
-    _id: activityId,
+    return postHTTP("/activity/approve", data, { withCredentials: true });
   };
 
-  if (accountType === "ASSOCIATION_ADMIN") {
-    body.association = await authService.getActiveAssociationID();
-  }
-
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Token: token,
-    },
-  };
-
-  let result = {};
-
-  let response = await axios.post(url, body, config).catch((err) => {
-    result.status = err.response.status;
-    result.data = err.response.data.message;
-
-    return result;
-  });
-  result.status = response.status;
-
-  if (response.status === 200) {
-    result.data = response.data;
-  } else {
-    result.data = response.data.message;
-  }
-
-  return result;
-};
-
-const approveActivity = async (activityId, approvedBy) => {
-  let token = await authService.getToken();
-  const url = baseUrl + "/approve";
-
-  const body = {
-    activityId: activityId,
-    approvedBy: approvedBy,
-  };
-
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Token: token,
-    },
-  };
-
-  let result = {};
-
-  let response = await axios.post(url, body, config).catch((err) => {
-    result.status = err.response.status;
-    result.data = err.response.data.message;
-
-    return result;
-  });
-  result.status = response.status;
-
-  result.data = response.data.message;
-
-  return result;
-};
-
-const addComment = async (text, activityId) => {
-  let token = await authService.getToken();
-  const url = baseUrl + "/comment/new";
-
-  const body = {
-    activity: activityId,
-    text: text,
-  };
-
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Token: token,
-    },
-  };
-
-  let result = {};
-
-  let response = await axios.post(url, body, config).catch((err) => {
-    result.status = err.response.status;
-    result.data = err.response.data.message;
-
-    return result;
-  });
-  result.status = response.status;
-
-  result.data = response.data.message;
-
-  return result;
-};
-
-const mediaUpload = async (file, mediaType, activityId) => {
-  let token = await authService.getToken();
-  const formData = new FormData();
-
-  formData.append("media", file);
-
-  const url = baseUrl + "/media/upload";
-
-  const config = {
-    headers: {
-      Token: token,
-    },
-    maxContentLength: Infinity,
-    maxBodyLength: Infinity,
-  };
-
-  let result = {};
-
-  let response = await axios.post(url, formData, config).catch((err) => {
-    console.log(err);
-    result.status = response.status;
-    result.data =
-      "We encountered an error while uploading your media. Please try again.";
-
-    return result;
-  });
-
-  if (response.status === 200) {
-    let urlToMedia = response.data.filePath;
-
-    const body = {
+  static addComment = (text, activityId) => {
+    const data = {
       activity: activityId,
-      urlToMedia: urlToMedia,
-      mediaType: mediaType,
+      text: text,
     };
 
-    const saveUrl = baseUrl + "/media/new";
+    return postHTTP("/activity/comment/new", data, { withCredentials: true });
+  };
 
-    const saveConfig = {
-      headers: {
-        "Content-Type": "application/json",
-        Token: token,
-      },
+  static mediaUpload = (file, mediaType, activityId) => {
+    const formData = new FormData();
+    formData.append("media", file);
+
+    return postHTTP("/activities/media/upload", formData, {
+      withCredentials: true,
+    }).then((res) => {
+      const data = {
+        activity: activityId,
+        urlToMedia: res.data.filePath,
+        mediaType: mediaType,
+      };
+
+      return postHTTP("/activities/media/new", data, { withCredentials: true });
+    });
+  };
+
+  static addActivityType = (activityType, association) => {
+    const data = {
+      association,
+      activityType,
     };
 
-    let saveResponse = await axios
-      .post(saveUrl, body, saveConfig)
-      .catch((err) => {
-        result.status = err.response.status;
-        result.data = err.response.data.message;
-
-        return result;
-      });
-    result.status = saveResponse.status;
-
-    result.data = saveResponse.data.message;
-
-    return result;
-  }
-};
-
-const addActivityType = async (activityType, association) => {
-  let token = await authService.getToken();
-  const url = baseUrl + "/type/new";
-
-  const body = {
-    association: association,
-    activityType: activityType,
+    return postHTTP("/activities/type/new", data, { withCredentials: true });
   };
 
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Token: token,
-    },
+  static getActivityTypes = (association) => {
+    const data = { association };
+
+    return postHTTP("/activities/type/get", data, { withCredentials: true });
   };
+}
 
-  let result = {};
-
-  let response = await axios.post(url, body, config).catch((err) => {
-    result.status = err.response.status;
-    result.data = err.response.data.message;
-
-    return result;
-  });
-  result.status = response.status;
-
-  result.data = response.data.message;
-
-  return result;
-};
-
-const getActivityTypes = async (association) => {
-  let token = await authService.getToken();
-  const url = baseUrl + "/type/get";
-
-  const body = {
-    association: association,
-  };
-
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Token: token,
-    },
-  };
-
-  let result = {};
-
-  let response = await axios.post(url, body, config).catch((err) => {
-    result.status = err.response.status;
-    result.data = err.response.data.message;
-    alert(result.data);
-    return result;
-  });
-  result.status = response.status;
-
-  if (response.status === 200) {
-    result.data = response.data;
-  } else {
-    result.data = response.data.message;
-  }
-
-  return result;
-};
-
-const functions = {
-  getActivities,
-  getActivityDetail,
-  approveActivity,
-  addComment,
-  mediaUpload,
-  addActivityType,
-  getActivityTypes,
-};
-
-export default functions;
+export default ActivityService;
